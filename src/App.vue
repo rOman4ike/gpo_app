3a5748e7da29a03cae0044c822286464e9e76b05
rOman4ike <rOman4ike@yandex.ru>
2023-05-18 16:01:00 +0700

    reset project

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,12 @@
 <template>
-  
-  <the-header></the-header>
-
-  <div class="common">
+  <div>
     <router-view />
   </div>
-
 </template>
 
 <script>
-import TheHeader from "./components/TheHeader.vue"
 
 export default {
-  components: {
-    TheHeader
-  }
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from "vue-router"
-import HomeView from "../views/HomeView.vue"
-import LoginView from "../views/LoginView.vue"
-import SignupView from "../views/SignupView.vue"
-import ProjectsView from "../views/ProjectsView.vue"
+// import HomeView from "../views/HomeView.vue"
+// import LoginView from "../views/LoginView.vue"
+// import SignupView from "../views/SignupView.vue"
+// import ProjectsView from "../views/ProjectsView.vue"
 
-import ReportsView from "../views/ReportsView.vue"
-import UsersView from "../views/UsersView.vue"
+// import ReportsView from "../views/ReportsView.vue"
+// import UsersView from "../views/UsersView.vue"
 
 
 const routes = [
-  { path: "/", name: "home", component: HomeView },
-  { path: "/signup", name: "signup", component: SignupView },
-  { path: "/login", name: "login", component: LoginView },
-  { path: "/projects", name: "projects", component: ProjectsView },
-  { path: "/reports", name: "reports", component: ReportsView },
-  { path: "/users", name: "users", component: UsersView },
+  // { path: "/", name: "home", component: HomeView },
+  // { path: "/signup", name: "signup", component: SignupView },
+  // { path: "/login", name: "login", component: LoginView },
+  // { path: "/projects", name: "projects", component: ProjectsView },
+  // { path: "/reports", name: "reports", component: ReportsView },
+  // { path: "/users", name: "users", component: UsersView },
 ];
 
 const router = createRouter({
